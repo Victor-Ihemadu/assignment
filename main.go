736810f294547f9e1524eb0349b669dcbc5570c8
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2023-02-05 21:03:03 +0100

    refactored println statement

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import "fmt"
 
-func manin() {
+func main() {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	for _, number := range numbers {
 		if number%2 != 0 {
-			fmt.Println("odd")
+			fmt.Println(number, "is odd")
 		} else {
-			fmt.Println("even")
+			fmt.Println(number, "is even")
 		}
 	}
 }
